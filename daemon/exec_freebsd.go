35766a04d78f7640db86b5d21baf870151b579bf
Alexey Guskov <lexag@mail.ru>
2015-05-19 17:45:56 +0300

    exec is supported on freebsd

--- daemon/exec_freebsd.go
+++ daemon/exec_freebsd.go
@@ -2,12 +2,8 @@
 
 package daemon
 
-import (
-  "errors"
-)
-
 // checkExecSupport returns an error if the exec driver does not support exec,
 // or nil if it is supported.
 func checkExecSupport(DriverName string) error {
-	return errors.New("not supported on bsd");
+	return nil
 }
