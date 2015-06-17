8d508eb783f5cae1ff2309be9e9ed1584bf4cea9
Alexey Guskov <lexag@mail.ru>
2015-06-17 10:24:43 +0300

    libnetwork and libcontainer are updated
    Signed-off-by: Alexey Guskov <lexag@mail.ru>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -18,7 +18,7 @@ clone git golang.org/x/net 3cffabab72adf04f8e3b01c5baf775361837b5fe https://gith
 clone hg code.google.com/p/gosqlite 74691fb6f837
 
 #get libnetwork packages
-clone git github.com/docker/libnetwork 3be488927db8d719568917203deddd630a194564
+clone git github.com/docker/libnetwork 279ffaeeaabdca373c49d0bfad684b40682a863e
 clone git github.com/docker/libkv e8cde779d58273d240c1eff065352a6cd67027dd
 clone git github.com/vishvananda/netns 5478c060110032f972e86a1f844fdb9a2f008f2c
 clone git github.com/vishvananda/netlink 8eb64238879fed52fd51c5b30ad20b928fb4c36c
@@ -30,7 +30,7 @@ clone git github.com/hashicorp/consul v0.5.2
 # get distribution packages
 clone git github.com/docker/distribution b9eeb328080d367dbde850ec6e94f1e4ac2b5efe
 
-clone git github.com/docker/libcontainer v2.1.1
+clone git github.com/docker/libcontainer ca3ab4ba400e66428f45efe0e88feae9b7ce72e1
 # libcontainer deps (see src/github.com/docker/libcontainer/update-vendor.sh)
 clone git github.com/coreos/go-systemd v2
 clone git github.com/godbus/dbus v2
